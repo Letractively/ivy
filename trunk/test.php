9992de122844b618e7d649b27dd2cdee9969f612
James Randell <James Randell@e269cd8b-576c-28c2-ea0d-9b7291f612aa>
2011-02-06 18:39:49 +0000

    Added in doc block and class level comments

--- trunk/test.php
+++ trunk/test.php
@@ -1,12 +1,18 @@
 <?php
 /**
- * Loader file
+ * Application start file, accessed from the browser
+ * 
+ * This file should share the same name as the name of the applications
+ * folder in ivy/application
  *
  * $Date$
- * $author: james.randell.user@googlemail.com $
+ * $Author$
  * 
  * $Revision$
- * 
+ */
+
+/**
+ * Call the start ivy file, and kick the whole process off
  */
 include_once 'ivy/core/controller/ivy.php';
 
